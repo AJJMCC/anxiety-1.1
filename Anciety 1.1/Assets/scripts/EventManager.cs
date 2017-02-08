4ad3ba7e2d6146823f512c2bb9ab619feba1ef42
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-02-08 20:29:54 +1000

    First Commit. Lighting, close door and mattress function. Room is build. Its frustrating so GG EZ

---
+++ Anciety 1.1/Assets/scripts/EventManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+    public static EventManager Instance;
+    [SerializeField]
+    private GameObject mattress;
+    private float smoothing = 7f;
+
+    public bool holdingglass = false;
+    public bool holdingbox = false;
+
+    public bool dooropen = false;
+    private float timer;
+
+    public Animator dooranim;
+    void Start ()
+    {
+        Instance = this;
+        timer = Random.Range(5,8);
+    }
+	
+
+	void Update ()
+    {
+        opendoor();
+        dooranim.SetBool("dooropen", dooropen);
+    }
+
+
+    public void opendoor()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            dooropen = true;
+        }
+        else dooropen = false;
+    }
+
+    public void Closedoor()
+    {
+        //reset the timer, tell the ray detect to stop showing the tooltip.
+        timer = Random.Range(5, 8);
+        raydetect.Instance.showbox = false;
+        Debug.Log("door closed");
+    }
+
+
+
+    public void Shiftmattress()
+    {
+        float mattressmove = 0.1f;
+        if (mattress.transform.position.x >= 0)
+        { mattressmove = Random.Range(-0.02f, -0.04f); }
+
+        else if (mattress.transform.position.x <= 0)
+        { mattressmove = Random.Range(0.02f, 0.04f); }
+
+
+        Vector3 newpos = new Vector3(mattressmove, mattress.transform.position.y,  mattress.transform.position.z);
+        StopCoroutine("Movement");
+        StartCoroutine("Movement", newpos);
+        //mattress.transform.position = Vector3.Lerp(mattress.transform.position, newpos, 1f);
+
+        Debug.Log("shifted mattress");
+    }
+
+
+
+    public void pickupglass()
+    {
+
+    }
+
+
+
+    public void pickupbox()
+    {
+
+    }
+
+    IEnumerator Movement(Vector3 target)
+    {
+        while (Vector3.Distance(mattress.transform.position, target) > 0.001f)
+        {
+            mattress.transform.position = Vector3.Lerp(mattress.transform.position, target, smoothing * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+}
---
+++ Anciety 1.1/Assets/scripts/raydetect.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raydetect : MonoBehaviour {
+
+    public static raydetect Instance;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private Texture2D image;
+
+
+    private float lookrange = 2f;
+
+
+    private Rect rect = new Rect(Screen.width / 2, Screen.height / 2, 15, 10);
+    private string message;
+    public bool showbox = false;
+
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+         Instance = this;
+        message = "demo text";
+	}
+	
+	void Update ()
+    {
+        HitObject();
+        //Debug.Log(HitObject());
+
+        objectChecks(HitObject());
+
+    }
+
+    string HitObject()
+    {
+        //raycast forward from camera to detect if we are close enough to an object to interact with it
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, lookrange))
+        {
+            return hit.collider.name.ToString();
+        }
+        else return null;
+    }
+
+    void objectChecks(string name)
+    {
+        //first check what we are looking at, then designate the GUI box and what happens if the player clicks. 
+        if (name == "door")
+        {
+
+            if (EventManager.Instance.dooropen == true)
+            {
+                showbox = true;
+                message = "close door";
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("clicked mouse");
+                    EventManager.Instance.Closedoor();
+                }
+            }
+           // else showbox = false;
+        }
+
+        else if (name == "box")
+        {
+            showbox = true;
+
+        }
+
+        else if (name == "glass")
+        {
+            
+            showbox = true;
+        }
+
+        else if (name == "mattress")
+        {
+            showbox = true;
+            message = "shift mattress";
+            if (Input.GetMouseButtonDown(0))
+            {
+                EventManager.Instance.Shiftmattress();
+            }
+        }
+        else { showbox = false; }
+
+    }
+
+    private void OnGUI()
+    {
+        //place cursor in centre
+        GUI.DrawTexture(rect, image, ScaleMode.StretchToFill);
+        if (showbox)
+        { GUI.Box(new Rect((Screen.width / 2) + 25, (Screen.height / 2) - 5, 100, 30), message); }
+
+    }
+}
